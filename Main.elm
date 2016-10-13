775f22d2d81b5ec6339dfc97b033f079cd09986e
Erich Grunewald <erichgrunewald@gmail.com>
2016-10-14 00:48:44 +0200

    Update game to use App.program; also, update model somewhat.

--- Main.elm
+++ Main.elm
@@ -1,47 +1,86 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.App
 
+
+-- Main
+
+
 main : Program Never
 main =
-  Html.App.beginnerProgram
-    { model = { score = 0, board = initialBoard }
-    , view = view
-    , update = update
-    }
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
-initialBoard : Board
-initialBoard =
-  []
+
 
 -- Model
+
+
+type Direction
+    = Left
+    | Right
+    | Up
+    | Down
+
+
+type alias Point =
+    { x : Float
+    , y : Float
+    }
+
+
 type alias Entity =
-  { x : Float
-  , y : Float
-  }
+    { position : Point
+    , direction : Direction
+    }
 
-type Cell =
-  Agent Entity
-  | Wall
-  | Food
-  | Empty
-
-type alias Board = List (List Cell)
 
 type alias Model =
-  { score : Int
-  , board : Board
-  }
+    { score : Int
+    , player : Entity
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { score = 0, player = { position = { x = 0, y = 0 }, direction = Right } }
+    , Cmd.none
+    )
+
+
 
 -- Update
-type Msg = NoOp
 
-update : Msg -> Model -> Model
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  model
+    ( model, Cmd.none )
+
+
 
 -- View
-view : Model -> Html.Html Msg
+
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ h1 [] [ text "FutuInvaders" ]
-    ]
+    div []
+        [ h1 [] [ text "FutuInvaders" ]
+        ]
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
