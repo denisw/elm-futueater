20ef00e9b6308d4e50c6b462bb4e3b04e5822e09
Erich Grunewald <erichgrunewald@gmail.com>
2016-10-15 10:51:02 +0200

    Implement a basic game with a moveable figure.

--- Main.elm
+++ Main.elm
@@ -1,7 +1,11 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.App
+import AnimationFrame exposing (diffs)
+import Keyboard exposing (downs)
+import Time exposing (inSeconds)
 
 
 -- Main
@@ -34,7 +38,7 @@ type alias Point =
     }
 
 
-type alias Entity =
+type alias Agent =
     { position : Point
     , direction : Direction
     }
@@ -42,7 +46,7 @@ type alias Entity =
 
 type alias Model =
     { score : Int
-    , player : Entity
+    , player : Agent
     }
 
 
@@ -58,12 +62,67 @@ init =
 
 
 type Msg
-    = NoOp
+    = Tick Float
+    | ChangeDirection Direction
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    let
+        player =
+            model.player
+    in
+        case msg of
+            Tick time ->
+                let
+                    moveDistance =
+                        playerSpeed * Time.inSeconds time
+
+                    newPlayer =
+                        moveAgent player moveDistance
+                in
+                    ( { model | player = newPlayer }, Cmd.none )
+
+            ChangeDirection direction ->
+                ( { model | player = { player | direction = direction } }, Cmd.none )
+
+
+{-| Takes an agent and a distance and moves the agent according to its
+direction.
+-}
+moveAgent : Agent -> Float -> Agent
+moveAgent agent distance =
+    let
+        newPosition =
+            case agent.direction of
+                Left ->
+                    shiftPoint agent.position -distance 0
+
+                Right ->
+                    shiftPoint agent.position distance 0
+
+                Up ->
+                    shiftPoint agent.position 0 distance
+
+                Down ->
+                    shiftPoint agent.position 0 -distance
+    in
+        { agent | position = newPosition }
+
+
+{-| Takes a point, a horizontal offset and a vertical offset and shifts the
+point according to the two given offsets.
+-}
+shiftPoint : Point -> Float -> Float -> Point
+shiftPoint point xOffset yOffset =
+    { x = point.x + xOffset, y = point.y + yOffset }
+
+
+{-| The player speed constant.
+-}
+playerSpeed : Float
+playerSpeed =
+    100
 
 
 
@@ -72,9 +131,39 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "FutuInvaders" ]
+    background
+        [ div []
+            [ player model
+            ]
         ]
+
+
+background : List (Html Msg) -> Html Msg
+background content =
+    div
+        [ style
+            [ ( "backgroundColor", "black" )
+            , ( "width", "100%" )
+            , ( "height", "100%" )
+            ]
+        ]
+        content
+
+
+player : Model -> Html Msg
+player model =
+    div
+        [ style
+            [ ( "backgroundColor", "yellow" )
+            , ( "width", "30px" )
+            , ( "height", "30px" )
+            , ( "borderRadius", "15px" )
+            , ( "position", "absolute" )
+            , ( "left", toString model.player.position.x ++ "px" )
+            , ( "top", toString model.player.position.y ++ "px" )
+            ]
+        ]
+        []
 
 
 
@@ -83,4 +172,25 @@ view model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    let
+        directionForKey keyCode =
+            case keyCode of
+                37 ->
+                    Left
+
+                38 ->
+                    Down
+
+                39 ->
+                    Right
+
+                40 ->
+                    Up
+
+                _ ->
+                    model.player.direction
+    in
+        Sub.batch
+            [ AnimationFrame.diffs Tick
+            , Keyboard.downs (\key -> ChangeDirection (directionForKey key))
+            ]
